12dd7f6f0616e11b2582fb450256ca7106250e15
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-21 13:50:13 -0600

    styling

--- frontend/sandbox/src/App.js
+++ frontend/sandbox/src/App.js
@@ -1,21 +1,49 @@
+import Navbar from './Shared/Navbar/Navbar'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
-function App() {
+export default function App() {
   return (
-    <div className = "container">
+    <>
+    <Navbar />
+    <div className = "container-fluid mt-5">
     <div className = "row">
-      <div className = "col col-md-4">
-          <p>ONE</p>
+      <div className = "col col-md-3">
+          <div className = "card">
+          <div className="card-header">
+      <DropdownButton id="dropdown-basic-button" title="My Files">
+        <Dropdown.Item href="#/action-1">New Folder</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">New File</Dropdown.Item>
+      </DropdownButton>
+
+     </div>
+            <div className = "card-body">
+              <p>Folders and files</p>
+            </div>
+          </div>
       </div>
-      <div className = "col col-md-4">
-          <p>TWO</p>
+      <div className = "col col-md-9">
+      <div className = "card">
+            <div className = "card-body">
+              <p>Code editor goes here</p>
+            </div>
+          </div>
       </div>
-      <div className = "col  col-md-4">
-          <p>THREE</p>
+    </div>
+    <div className='row mt-3'>
+      <div className='col-md-12'>
+        <div className='card'>
+          <div className='card-body'>
+            <h5 className = "card-title"> Console </h5>
+          </div>
+
+        </div>
+
       </div>
     </div>
     </div>
+    </>
   );
 }
 
-export default App;
